92f08154096045a4cb854394dff489bef0320ebd
bwdurst <bwdurst@gmail.com>
2019-10-03 17:43:56 -0500

    centering contact form

---
+++ src/animate-tutorial/animations.js
@@ -0,0 +1,7 @@
+function openNav() {
+  //this is where I do stuff
+  //show me nav-item--mobile
+  let navMobile = document.querySelector('.nav-items--mobile')
+  navMobile.classList.toggle('showNav')
+  console.log('navMobile:', navMobile)
+}
